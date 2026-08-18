6a93cfc6238db0b460119b40159af46556ed99be
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-18 13:20:40 -0700

    Daily C++ practice

---
+++ Day-01/02-Sphere-Volume.cpp
@@ -0,0 +1,13 @@
+#include <iostreamecho.
+using namespace std;
+
+int main() {
+    float r;
+
+    cout << "Enter Radius of Sphere : ";
+    cin >> r;
+
+    cout << "Volume Of Sphere Is : " << (4.0 / 3.0) * 3.1415 * r * r * r;
+
+    return 0;
+}
