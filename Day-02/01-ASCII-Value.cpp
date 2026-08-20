3f203d171edb607f93ce341598f27ae3b4eb59e7
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-20 10:40:35 -0700

    Daily C++ practice

---
+++ Day-02/01-ASCII-Value.cpp
@@ -0,0 +1,9 @@
+#include <iostreamecho using namespace std;
+int main() {
+    char ch;
+    cout << "ASCII VALUES" << endl;
+    cout << "Enter Character : ";
+    cin >> ch;
+    cout << "ASCII VALUE OF " << ch << " IS : " << (int)ch;
+    return 0;
+}
