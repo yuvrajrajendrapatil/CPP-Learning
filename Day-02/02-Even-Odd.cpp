549e9b30100284d7d2a67e97af0008707ac534f0
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-22 21:31:09 +0530

    Daily C++ practice

--- Day-02/02-Even-Odd.cpp
+++ Day-02/02-Even-Odd.cpp
@@ -1,10 +1,15 @@
-#include <iostreamecho using namespace std;
+#include <iostream>
+using namespace std;
 
 int main() {
     int n;
     cout << "Enter a Number : " << endl;
     cin >> n;
-    if(n %% 2 == 0) cout << "It is Even Number";
-    else cout << "It is Odd Number";
+
+    if(n % 2 == 0)
+        cout << "It is Even Number";
+    else
+        cout << "It is Odd Number";
+
     return 0;
 }
