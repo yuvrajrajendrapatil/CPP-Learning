94270169bd78adbbe4440cbed6610434d97bbf9f
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-25 20:57:52 +0530

    Daily C++ practice

---
+++ Day-02/04-Absolute-Value.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter Integer : ;
+ cin >> a;
+ if(a < 0) {
+ cout << Absolute Value Of Given Integer :  << a * (-1);
+ }
+ else {
+ cout << Absolute Value Of Given Integer :  << a;
+ }
+ return 0;
+}
