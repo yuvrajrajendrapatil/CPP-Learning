aa86cac13884dab14caeca8f7540eea0b004041c
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-25 20:45:14 +0530

    Daily C++ practice

---
+++ Day-02/03-Age-Category.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter your age : ;
+ cin >> a;
+ if(a >= 18) {
+ cout << Adult;
+ }
+ else if(a >= 13) {
+ cout << Teenager;
+ }
+ else {
+ cout << Child;
+ }
+
+ return 0;
+}
