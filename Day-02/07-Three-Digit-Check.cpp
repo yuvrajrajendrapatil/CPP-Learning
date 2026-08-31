1f074fa006f694863c403102a8a429697e070190
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-31 22:57:32 +0530

    Daily C++ practice

---
+++ Day-02/07-Three-Digit-Check.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a Positive Number I will Tell You It is Three digit Or Not : ;
+ cin >> n;
+ if(n > 99 && n < 1000)
+ {
+ cout << It Is Three Digit Number;
+ }
+ else
+ {
+ cout << It Is Not Three Digit Number;
+ }
+ return 0;
+}
