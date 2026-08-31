fb3a40f4a87ed7c5bc4b3db66ff1171723cb8e7d
Yuvraj Patil <yuvrajrajendrapatil@gmail.com>
2026-08-31 23:02:52 +0530

    Daily C++ practice

---
+++ Day-02/08-Divisible-By-5-And-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a Positive Number I will Tell U Divisible by 5 and 3 : ;
+ cin >> n;
+ if(n % 5 == 0 && n % 3 == 0)
+ {
+ cout << It Is Divisible By 5 and 3;
+ }
+ else
+ {
+ cout << It Is Not Divisible By 5 and 3;
+ }
+ return 0;
+}
